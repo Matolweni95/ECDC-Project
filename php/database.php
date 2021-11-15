8bb67be145e85b0a786b035e361e5809a9718c90
Lisa <masixolematolweni.1@gmail.com>
2021-11-15 15:55:54 +0200

    test commit

--- php/database.php
+++ php/database.php
@@ -11,6 +11,6 @@ $db_name = 'mountainviewdb';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 
 if ($mysqli->connect_error) {
-die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+die('Error : ('. $mysqli->connect_errno . ') '. $mysqli->connect_error);
 }
 ?>
