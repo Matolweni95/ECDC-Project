60d1afa9308e4fdd4486cddde3f259ea8ee77e94
masixolematolweni.1@gmail.com <72172920+Matolweni95@users.noreply.github.com>
2021-11-16 11:26:59 +0200

    update

---
+++ src/app/parentPortal/mychild-profile/mychild-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mychild-profile',
+  templateUrl: './mychild-profile.component.html',
+  styleUrls: ['./mychild-profile.component.css']
+})
+export class MychildProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
