1767391f23a32ff5df8639863cf3ff81d732ffb8
masixolematolweni.1@gmail.com <72172920+Matolweni95@users.noreply.github.com>
2021-11-17 23:37:19 +0200

    index

--- src/app/mainSite/home-section/home-section.component.ts
+++ src/app/mainSite/home-section/home-section.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-section',
-  templateUrl: './home-section.component.html',
+  templateUrl: './index.html',
   styleUrls: ['./home-section.component.css']
 })
 export class HomeSectionComponent implements OnInit {
